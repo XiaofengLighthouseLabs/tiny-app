badf8e0d55fc8750eb26507dbd1c492bdde6a53d
trrippy <wes.wong@hotmail.com>
2017-01-17 22:32:58 +0000

    created new URL page, add err pg for urls not in db

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -9,16 +12,37 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+// GET
 app.get("/", (req, res) => {
   res.end("Hello!");
+});
+
+app.get('/urls', (req,res) => {
+  let templateVars = { url: urlDatabase };
+  res.render('urls_index', templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id };
+  if (urlDatabase.hasOwnProperty(req.params.id)){
+    templateVars.fullUrl = urlDatabase[req.params.id];
+  } else templateVars.fullUrl = "Url not in database."
+  res.render("urls_show", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
+// POST
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
